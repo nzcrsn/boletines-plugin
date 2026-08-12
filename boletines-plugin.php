487a861079cd8aeb780fc13206d0d84a8abede39
Bryan Nuñez <carvajalbryan@hotmail.com>
2026-08-12 00:15:46 -0500

    chore: initialize project

---
+++ boletines-plugin.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: Boletines Plugin
+ * Description: Newsletter management and display system.
+ * Version: 1.0.0 | By nzcrsn
+ */
+
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+
